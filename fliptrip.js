8393cea4254fcfc36e5bdc2b3f8ce810ccd2ea41
Masahide Maehara <maehrm@gmail.com>
2019-07-31 21:44:55 +0900

    数学ガールのリンクの追加

--- fliptrip.js
+++ fliptrip.js
@@ -4,36 +4,29 @@ var pattern = new Array(2**N);
 var finish_flag;
 var error_flag;
 var canvas;
-var reset_button;
+var w, h;
 
 function setup() {
     init();
-    canvas = createCanvas(N * 50, 200);
+    w = windowWidth / 2;
+    h = windowHeight / 3;
+    canvas = createCanvas(w, h);
     canvas.parent("FlipTripCanvas");
-    reset_button = createButton("Reset!");
-    reset_button.position(windowWidth / 2 - (reset_button.width / 2),
-                          height);// - reset_button.height / 2);
-    reset_button.style("width",  "50px");
-    reset_button.style("height", "30px");
-    reset_button.style("border-radius", "5px");
-    reset_button.mousePressed(function() {
-        init();
-    });
 }
 
 function draw() {
-    //background('pink');
-    background(255, 255, 255);
+    background('pink');
+    //background(255, 255, 255);
     strokeWeight(2);
     stroke(128, 128, 128);      // 輪郭線
     for (var i = 0; i < N; i++) {
         // 数字の描画
         fill(0);
         textSize(20);
-        text(N - (i + 1), i * 50 + 20, 65);
+        text(N - (i + 1), w / 6 + i * w / 6 + w / 12, 65);
         // オセロ盤の描画
         fill(0, 200, 0);
-        rect(i * 50, 75, 50, 50);
+        rect(w / 6 + i * w / 6, 75, w / 6, w / 6);
         // オセロの描画
         if (board[i]) {
             fill(0, 0, 0);                  
@@ -41,29 +34,28 @@ function draw() {
         else {
             fill(255, 255, 255);
         }
-        ellipse(i * 50 + 25, 100, 42, 42);
+        ellipse(w / 6 + i * w / 6 + w / 12, w / 6 + 75 - w / 12, w / 7);
     }
     if (error_flag) {
         fill(0);
         textSize(20);
         textAlign(CENTER);
-        text("ERROR!", width / 2, 25);
+        text("ERROR!", w / 2, 4 * h / 5);
     }
     if (finish_flag) {
         fill(0);
         textSize(20);
         textAlign(CENTER);
-        text("FULLTRIP!", width / 2, 25);
+        text("FULLTRIP!", w / 2, 3 * h / 4);
     }              
-
 }
 
 function mousePressed() {
-    var x = Math.floor(mouseX / 50);
+    var x = Math.floor(mouseX / (w / 6)) - 1;
     var y = mouseY;
     var n = 0;
     if (finish_flag || error_flag) return;
-    if (x >= 0 && x < N && y >= 75 && y <= 125) {
+    if (x >= 0 && x < N && y >= 75 && y <= 150) {
         board[x] = 1 - board[x];
         for (var i = 0; i < N ; i++) {
             n += 2**(N - (i + 1)) * board[i];
@@ -95,3 +87,8 @@ function init() {
     }
     pattern[0] = true;
 }
+
+document.querySelector("button").addEventListener("click",
+                                                  function() {
+                                                      init();
+                                                  });
